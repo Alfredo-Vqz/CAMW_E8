12e881addc0c90b64519ebf49e30ecbba2d14ff9
Alfredo-Vqz <a01656384@tec.mx>
2023-09-11 14:01:18 -0600

    descanso

--- front/src/components/Example.tsx
+++ front/src/components/Example.tsx
@@ -1,9 +1,36 @@
-function Example() {
-    return (
-        <h1>
-            hola mundo from example 
-        </h1>
-    );
-}
+import React, { useState } from "react";
+import "./Button.css"
 
+function Example(){
+    const [contador,setContador]=useState(0);
+    const [name,setName]=useState("");
+    function sumaContador(){
+        console.log("Llamando a la funcion sumar")
+        setContador(contador+1);
+        console.log("Terminando la funcion sumar")
+    }
+
+    function onChangeInput(event:any){
+        const {value}=event.target
+        setName(value)
+    }
+
+
+    return (
+        <div>
+            {name==="" &&
+                <h1>Whats your Name?</h1>
+            }
+            {name!=="" &&
+                <h1>Hello {name}, your count is {contador}</h1>
+            }
+            <input type="text" onChange={onChangeInput} name="name" value={name} />
+            {name!=="" &&
+                <button onClick={sumaContador} className="Button">Sumar</button>
+            }
+        </div>
+    );
+
+
+}
 export default Example;
