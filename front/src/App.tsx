8ea972b5e66a07a082e8a36595d8730ffb7debd9
Esteban Zepeda <109043513+estzep@users.noreply.github.com>
2023-09-11 14:56:22 -0600

    Update

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Example from './components/Example';
+import Zelda from './components/Zelda';
 
 function App() {
   return (
     <div className="App">
         <Example></Example> 
+        <Zelda></Zelda>
     </div>
   );
 }
